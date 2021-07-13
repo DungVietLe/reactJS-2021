25f66bab8c46715e424159bc2c02048946877f77
dung <dungvietle@gmail.com>
2021-07-13 17:33:12 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+// import Todoitem from './components/Todoitem';
+import React,{ Component } from 'react';
+import Todoitem from './components/Todoitem';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.todoItems = ['Mua bim bim', 'di da bong','di do xang'];
+
+  }
+  render() {
+    return (
+      <div className="App">
+   
+         {
+           this.todoItems.map((item,index)=> <Todoitem key={index} title={item}/>)
+         }
+      
+      
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Todoitem.js
@@ -0,0 +1,14 @@
+// import React, { Component } from 'react';
+import React, {Component} from 'react';
+class Todoitem extends Component {
+    render() {
+     
+        return (
+            <div className="Todoitem"> 
+
+            <p>{this.props.title}</p>
+            </div>
+        )
+    }
+}
+export default Todoitem
